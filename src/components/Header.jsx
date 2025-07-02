579f0dc999aeafd0216c6a659062be4e2138f6cc
Luchintha Anjana <luchinthaa@gmail.com>
2025-07-02 09:10:07 +0530

    Navbar Completed

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <header>
+            <img className="globe-marker" src="./src/assets/images/globe.png" />
+            <h1 className="navbar-h1">Travel Journal</h1>
+        </header>
+    )
+}
